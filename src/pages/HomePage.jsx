9d85a778a4e2b8b30086c3009d136acef580b5a5
NatiEfraim <144374682+NatiEfraim@users.noreply.github.com>
2023-12-11 21:39:53 +0200

    Start Design Menu Component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,30 @@
 import React, { Component, Fragment } from "react";
 import FeaturedProducts from "../components/home/FeaturedProducts";
+import NavMenuDesktop from "../components/common/NavMenuDesktop";
+
 import Categories from "../components/home/Categories";
 import Collection from "../components/home/Collection";
 import NewArrival from "../components/home/NewArrival";
+import HomeTop from "../components/home/HomeTop";
 
 export class HomePage extends Component {
   render() {
     return (
       <Fragment>
+        <NavMenuDesktop />
+        <HomeTop />
         <FeaturedProducts />
         <NewArrival />
         <Collection />
         <Categories />
       </Fragment>
+      // <Fragment>
+      //   <HomeTop />
+      //   <FeaturedProducts />
+      //   <NewArrival />
+      //   <Categories />
+      //   <Collection />
+      // </Fragment>
     );
   }
 }
