38ff88ecb124961badf7f3024f210f0be285fd54
NatiEfraim <144374682+NatiEfraim@users.noreply.github.com>
2023-12-16 23:50:37 +0200

    Complete Contacts & Visitor

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,11 +10,17 @@ import Categories from "../components/home/Categories";
 import Collection from "../components/home/Collection";
 import NewArrival from "../components/home/NewArrival";
 import HomeTop from "../components/home/HomeTop";
+import AppURL from "../api/AppURL";
+import axios from "axios";
 
 export class HomePage extends Component {
   componentDidMount() {
     window.scroll(0, 0);
+    this.GetVisitorDetails();
   }
+  GetVisitorDetails = () => {
+    axios.get(AppURL.VisitorDetails).then().catch();
+  };
 
   render() {
     return (
---
+++ src/validation/validation.jsx
@@ -0,0 +1,6 @@
+class validation {
+  static NameRegx = /^[A-Za-z\'\s\.\:\-]+$/;
+  static EmailRegx = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+}
+
+export default validation;
