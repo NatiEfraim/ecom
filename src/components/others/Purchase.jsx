aa11e19763e9427091d38581e68df854c23bdbdb
NatiEfraim <144374682+NatiEfraim@users.noreply.github.com>
2023-12-20 20:44:00 +0200

    Complete SiteInfo From SQL

--- src/components/others/Purchase.jsx
+++ src/components/others/Purchase.jsx
@@ -3,6 +3,8 @@ import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import AppURL from "../../api/AppURL";
 import axios from "axios";
 import ReactHtmlParser from "react-html-parser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 export class Purchase extends Component {
   constructor() {
     super();
@@ -12,16 +14,32 @@ export class Purchase extends Component {
   }
 
   componentDidMount() {
-    axios
-      .get(AppURL.AllSiteInfo)
-      .then((response) => {
-        let StatusCode = response.status;
-        if (StatusCode == 200) {
-          let JsonData = response.data[0]["parchase_guide"];
-          this.setState({ purchase: JsonData });
-        }
-      })
-      .catch((error) => {});
+    let SiteInfoPurchase = sessionStorage.getItem("AllSiteInfo");
+    if (SiteInfoPurchase == null) {
+      axios
+        .get(AppURL.AllSiteInfo)
+        .then((response) => {
+          let StatusCode = response.status;
+          if (StatusCode == 200) {
+            let JsonData = response.data[0]["parchase_guide"];
+            this.setState({ purchase: JsonData });
+
+            sessionStorage.setItem("SiteInfoPurchase", JsonData);
+          } else {
+            toast.error("Somthing Went Wrong", {
+              position: "bottom-center",
+            });
+          }
+        })
+        .catch((error) => {
+          toast.error("Somthing Went Wrong", {
+            position: "bottom-center",
+          });
+        });
+    } // end If Conditon
+    else {
+      this.setState({ purchase: SiteInfoPurchase });
+    }
   }
 
   render() {
